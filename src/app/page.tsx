fbe50db5fd12a4144a2c17ce0c6b045ca0797b41
yihyun-kim1 <hyunnyfarm@naver.com>
2024-03-12 21:35:48 +0900

    + 버튼을 통해 입력한 값으로 새로운 div를 생성하는 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
-import Image from "next/image";
+'use client'
+import { useState } from 'react';
+import './globals.css';
 
 export default function Home() {
+  const [todoItem, setTodoItem] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTodoItem = () => {
+    if (inputValue.trim() !== '') {
+      setTodoItem([...todoItem, inputValue]);
+      setInputValue('');
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col bg-white items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full bg-black text-white items-center justify-between font-mono text-sm lg:flex">
-focus today
+
+    <main className="flex min-h-screen flex-col bg-white ">
+      <div className="py-[100px] mx-[200px] w-[360px] ">
+        <h1 className='text-[40px]'>
+        Hello,<br/>
+        let's focus today.</h1>
+        <div className='flex w-[360px] mt-[50px]'>
+          <div className='flex flex-row'>
+            <textarea className='flex w-[240px] h-[50px] border-1 border-black text-gray-600 text-center'  
+                      value={inputValue}
+                      onChange={handleInputChange}></textarea>
+            <button className='w-[50px] h-[50px] bg-black rounded-lg text-white text-[30px]' onClick={addTodoItem}>+</button>
+          </div>
+        </div>
+        {todoItem.map((item, index) => (
+          <div key={index} className="border-1 w-[240px] h-[50px] border-gray-600 mt-4">
+            {item}
+          </div>
+        ))}
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
       },
     },
   },
