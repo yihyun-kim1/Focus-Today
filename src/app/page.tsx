ffa10486994039665d619ac51f4ca1d411a76b7c
yihyun-kim1 <hyunnyfarm@naver.com>
2024-03-12 21:54:16 +0900

    modal 추가 및 내부에서 지정한 옵션을 반영하도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import './globals.css';
 export default function Home() {
   const [todoItem, setTodoItem] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>('');
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [selectedColor, setSelectedColor] = useState<string>('black');
+  const [selectedTime, setSelectedTime] = useState<number>(10);
+
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue(event.target.value);
@@ -12,9 +16,15 @@ export default function Home() {
 
   const addTodoItem = () => {
     if (inputValue.trim() !== '') {
-      setTodoItem([...todoItem, inputValue]);
+      setTodoItem([...todoItem, `${selectedTime}분 ${selectedColor} - ${inputValue}`]);
       setInputValue('');
+      setShowModal(false)
     }
+  };
+
+  const handleDeleteAll = () => {
+    setTodoItem([]);
+    setShowModal(false);
   };
 
   return (
@@ -29,7 +39,7 @@ export default function Home() {
             <textarea className='flex w-[240px] h-[50px] border-1 border-black text-gray-600 text-center'  
                       value={inputValue}
                       onChange={handleInputChange}></textarea>
-            <button className='w-[50px] h-[50px] bg-black rounded-lg text-white text-[30px]' onClick={addTodoItem}>+</button>
+            <button className='w-[50px] h-[50px] bg-black rounded-lg text-white text-[30px]' onClick={() => setShowModal(true)}>+</button>
           </div>
         </div>
         {todoItem.map((item, index) => (
@@ -37,6 +47,69 @@ export default function Home() {
             {item}
           </div>
         ))}
+        {showModal && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-8 w-[360px] h-[400px] flex flex-col">
+            <h2 className="text-lg font-semibold mb-4">컬러</h2>
+            <div className="flex flex-row mb-4">
+              <button
+                className={`flex-1 w-[30px] h-[30px] bg-black text-white mr-2 ${selectedColor === 'black' ? 'border-2 border-gray-800' : ''}`}
+                onClick={() => setSelectedColor('black')}
+              >
+              </button>
+              <button
+                className={`flex-1 bg-pink-500 text-white mr-2 ${selectedColor === 'pink' ? 'border-2 border-gray-800' : ''}`}
+                onClick={() => setSelectedColor('pink')}
+              >
+              </button>
+              <button
+                className={`flex-1 bg-orange-500 text-white mr-2 ${selectedColor === 'orange' ? 'border-2 border-gray-800' : ''}`}
+                onClick={() => setSelectedColor('orange')}
+              >
+              </button>
+              <button
+                className={`flex-1 bg-yellow-500 text-white mr-2 ${selectedColor === 'yellow' ? 'border-2 border-gray-800' : ''}`}
+                onClick={() => setSelectedColor('yellow')}
+              >
+              </button>
+              <button
+                className={`flex-1 bg-green-500 text-white ${selectedColor === 'green' ? 'border-2 border-gray-800' : ''}`}
+                onClick={() => setSelectedColor('green')}
+              >
+              </button>
+            </div>
+            <div className="flex flex-col">
+              <h2 className="text-lg font-semibold">시간</h2>
+              <div className='flex flex-row'>
+              {[10, 15, 20, 25, 30, 45, 55].map((time) => (
+                <button
+                  key={time}
+                  className={`flex-1 mt-[10px] bg-white border border-gray-800 mr-2 ${selectedTime === time ? 'bg-gray-300' : ''}`}
+                  onClick={() => setSelectedTime(time)}
+                >
+                  {time}
+                </button>
+              ))} 
+              </div>
+              <textarea placeholder='직접입력'></textarea>
+            </div>
+            <div className="flex flex-row mt-[20px]">
+              <button
+                className="flex-1 bg-gray-300 text-black mr-2"
+                onClick={handleDeleteAll}
+              >
+                Delete
+              </button>
+              <button
+                className="flex-1 bg-gray-300 text-white"
+                onClick={addTodoItem}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       </div>
     </main>
   );
